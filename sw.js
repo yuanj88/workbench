b4ef24b95de9a0a19c71ad2073f83196cadba8e6
yuanj88 <79256525+yuanj88@users.noreply.github.com>
2026-07-30 20:25:30 +0800

    修复bug

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wb-pwa-v1';
+const CACHE='wb-pwa-v2';
 const ASSETS=['.','index.html','manifest.webmanifest','icon-192.png','icon-512.png','apple-touch-icon.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
